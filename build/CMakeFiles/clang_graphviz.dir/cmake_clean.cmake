63811f5993397e7607d6fda1ddc767661a49dc2c
Sbogov <caitalar80@gmail.com>
2024-01-22 01:03:36 +0300

    upload releaese buid project

---
+++ build/CMakeFiles/clang_graphviz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clang_graphviz.dir/src/clang_graphviz.c.o"
+  "CMakeFiles/clang_graphviz.dir/src/clang_graphviz.c.o.d"
+  "clang_graphviz"
+  "clang_graphviz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clang_graphviz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
